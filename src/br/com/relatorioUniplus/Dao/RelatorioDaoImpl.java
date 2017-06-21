3280b570e9db77b3ddc6979b26ed94671db73537
casa <casa@SOPHOS4-PC>
2017-06-21 16:34:06 -0300

    daoimpl implementado

--- src/br/com/relatorioUniplus/Dao/RelatorioDaoImpl.java
+++ src/br/com/relatorioUniplus/Dao/RelatorioDaoImpl.java
@@ -3,13 +3,14 @@ package br.com.relatorioUniplus.Dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import br.com.relatorioUniplus.Factory.ConnectionFactory;
 import br.com.relatorioUniplus.model.Relatorio;
 
 public class RelatorioDaoImpl extends ConnectionFactory implements relatorioDao {
 	private static final String TABLE = "notafiscal";
-	private static final String COLUMNS = "";
+	private static final String COLUMNS = "numeronotafiscal";
 	
 	@Override
 	public Relatorio buscar(int id) {
@@ -18,8 +19,27 @@ public class RelatorioDaoImpl extends ConnectionFactory implements relatorioDao 
 		ResultSet rs = null;
 		StringBuilder sql = new StringBuilder();
 		
-		
-		return null;
+		sql.append("SELECT * FROM");
+		sql.append(TABLE);
+		sql.append(" ");
+		sql.append("WHERE numeronotafiscal = ?");
+		try {
+			con = getConnection();
+			Relatorio relatorio = new Relatorio();
+			ps = con.prepareStatement(sql.toString());
+			rs = ps.executeQuery();
+			
+			rs.next();
+			relatorio.setNumeronotafiscal(rs.getInt("numeronotafical"));
+			
+			ps.close();
+			rs.close();
+			con.close();
+			return relatorio;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("erro ao buscar funcionario", e);
+		}
 	}
 
 }
